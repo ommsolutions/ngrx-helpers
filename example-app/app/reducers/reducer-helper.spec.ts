36c5ac52d941342207a9fc88b4e7ee7513046fba
Andreas Enenkel <andreas.enenkel@gmail.com>
2018-03-03 17:34:37 +0100

    Adds basic loadAll Test to verify generic reducer

---
+++ example-app/app/reducers/reducer-helper.spec.ts
@@ -0,0 +1,49 @@
+/**
+ * Static test for the library test code
+ */
+import {ISuccessAction, ReducerHelper} from "@omm/ngrx-helpers";
+import {IPlant, PlantsResource} from "../resources/plants.resource";
+import {EntityState} from "@ngrx/entity";
+
+describe("reducer-helper", () => {
+    let plantResource: PlantsResource;
+    const plants: IPlant[] = [{
+        "id": 1,
+        "city": "Devonstad",
+        "zipCode": "78179",
+        "street": "Audie Glens",
+        "country": "Lesotho",
+        "description": "provident fugiat voluptas"
+    }, {
+        "id": 2,
+        "city": "North Jadaton",
+        "zipCode": "13812-8612",
+        "street": "Idell Passage",
+        "country": "Saint Pierre and Miquelon",
+        "description": "est dignissimos et"
+    }];
+
+    beforeAll(() => {
+        plantResource = new PlantsResource();
+
+    });
+
+
+    it("Should add all", () => {
+        const LoadAllSuccessAction: ISuccessAction = {
+            action: "LoadAll",
+            type: plantResource.getActionType("LoadAll", "success"),
+            payload: undefined,
+            response: plants
+        };
+        const state: EntityState<IPlant> = ReducerHelper.genericReducer(plantResource.entityAdapter.getInitialState(),
+            LoadAllSuccessAction,
+            PlantsResource);
+        expect(state.ids).toBeTruthy();
+        expect(state.ids.length).toEqual(2);
+
+        const plant = state.entities[state.ids[0]];
+        expect(plant).toBeTruthy();
+        expect(plant).toEqual(plants[0]);
+    });
+});
